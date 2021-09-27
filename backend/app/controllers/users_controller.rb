f0c5a75472b6d72848474efbc0a715084be9c5fc
Lrd134 <larryc3200@gmail.com>
2021-09-27 17:24:26 -0400

    added serialize_user method

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
 
   def show
     user = User.find_by(name: params[:id])
-    render json: UserSerializer.new(user).serialized_json
+    render json: serialize_user(user)
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      render json: UserSerializer.new(user).serialized_json
+      render json: serialize_user(user)
     else
       render json: "error"
     end
@@ -31,6 +31,10 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :highscore)
+  end
+
+  def serialize_user(user)
+    UserSerializer.new(user).serialized_json
   end
 
 end
