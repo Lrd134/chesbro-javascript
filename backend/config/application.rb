2d0404b81410b871fc458e6285394965ded9e054
Lrd134 <larryc3200@gmail.com>
2021-09-27 17:52:40 -0400

    Added rack-cors

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -23,7 +23,16 @@ module ChesbroJavascript
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
