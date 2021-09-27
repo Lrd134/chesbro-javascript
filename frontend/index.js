582f507bc5b11c3a4c45afbeddb163578843d618
Lrd134 <larryc3200@gmail.com>
2021-09-27 17:49:09 -0400

    added create user functionality

--- frontend/index.js
+++ frontend/index.js
@@ -16,8 +16,22 @@ const createUserConfig = (userName, userHighscore = 0) => {
   }
 }
 
-document.addEventListener("keydown", e =>{
-  fetch("http://localhost:3000/users", createNewUserConfig("Faux name for now")).then(resp => resp.json()).then(json => console.log(json)).catch(error => console.log("error" + error));
+const addEvents = () => {
+  document.getElementsByClassName('create-user')[0].addEventListener("submit", e => {
+    e.preventDefault();
+    const userName = document.getElementsByClassName('new-user-name')[0].value;
+    console.log("Before Fetch")
+    fetch("http://localhost:3000/users", createUserConfig(userName)).then(resp => resp.json()).then(json => console.log(json)).catch(error => console.log("error" + error));
+    console.log("After fetch and before prevent default")
+    
+    console.log("After preventDefault");
+  })
+}
+document.addEventListener("DOMContentLoaded", e =>{
+  addEvents();
 })
 
+
+// 
+// 
 
