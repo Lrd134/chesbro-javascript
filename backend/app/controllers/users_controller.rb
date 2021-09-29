d284e1ee4e58aae8fdcd4ca26d61e2bb084d6bc8
Lrd134 <larryc3200@gmail.com>
2021-09-29 17:00:47 -0400

    updated users#update

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find_by(id: params[:id])
+    user = User.find_by(name: params[:name])
     if user.update(user_params)
       render json: "User Updated Successfully"
     else
