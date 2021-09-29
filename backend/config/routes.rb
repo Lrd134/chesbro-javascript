6e11317180aa9f1a3c1d07a8f8095a13a182d37a
Lrd134 <larryc3200@gmail.com>
2021-09-29 17:10:46 -0400

    updated routes for users

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
+  resources :users, except: %i[show update]
+  scope '/users' do 
+    get '/:name', to: 'users#show'
+    post '/:name', to: 'users#update'
+  end
 end
