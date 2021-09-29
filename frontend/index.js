cf5844ed42cc369cb72eca6b741f55bed7c4e552
Lrd134 <larryc3200@gmail.com>
2021-09-29 16:03:55 -0400

    removed getUser method

--- frontend/index.js
+++ frontend/index.js
@@ -25,11 +25,6 @@ class User {
   static fromJson(json) {
     return new User(parseInt(json.data.id, 10), json.data.attributes.name, json.data.attributes.highscore)
   }
-  static getUser(name) {
-    let user;
-    fetch(indexUrl + `users/${name}`).then(resp => resp.json()).then(json => user = User.fromJson(json))
-    return user;
-  }
   static getUsers() {
     fetch(indexUrl + "users").then(resp => resp.json()).then(json => User.renderUsers(json)).catch(error => error);
   }
