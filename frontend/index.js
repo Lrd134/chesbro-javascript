76f77f5ee022ee58c96f414aee41c9f0e9739057
Lrd134 <larryc3200@gmail.com>
2021-09-29 06:36:33 -0400

    fitted some login into the User class

--- frontend/index.js
+++ frontend/index.js
@@ -11,25 +11,11 @@ const addEvents = () => {
     console.log("After preventDefault");
   })
 }
-const fetchUsersIndex = () => {
-  fetch(indexUrl + "users").then(resp => resp.json()).then(json => renderHighscores(User.createUsers(json))).catch(error => error);
-}
-const renderHighscores = (arrayOfUsers) => {
-  const users = arrayOfUsers;
-  const highscoreDiv = document.getElementsByClassName('highscore')[0];
-  const ul = document.createElement('ul');
-  users.map(e => {
-    const li = document.createElement('li');
-    li.classList.add('highscore-actual')
-    li.innerText = `${e.name} has a highscore of ${e.highscore}.`
-    ul.appendChild(li);
-  })
-  highscoreDiv.appendChild(ul);
-  
-}
+
+
 document.addEventListener("DOMContentLoaded", e =>{
   addEvents();
-  fetchUsersIndex();
+  User.getUsers();
 })
 
 
@@ -37,17 +23,38 @@ document.addEventListener("DOMContentLoaded", e =>{
 // 
 
 class User {
+
   constructor(id, name = "Example", highscore = 0) {
     this.id = id;
     this.name = name;
     this.highscore = highscore;
   }
+  
+  static getUsers() {
+    fetch(indexUrl + "users").then(resp => resp.json()).then(json => User.createUsers(json)).catch(error => error);
+  }
+
   static createUsers(jsonUsers) {
     const users = [];
     jsonUsers.data.forEach(e =>{
       users.push(new User(e.id, e.attributes.name, e.attributes.highscore))
     });
+    this.renderHighscores(users);
     return users;
+
+  }
+  static renderHighscores(arrayOfUsers) {
+    const users = arrayOfUsers;
+    const highscoreDiv = document.getElementsByClassName('highscore')[0];
+    const ul = document.createElement('ul');
+    users.map(e => {
+      const li = document.createElement('li');
+      li.classList.add('highscore-actual')
+      li.innerText = `${e.name} has a highscore of ${e.highscore}.`
+      ul.appendChild(li);
+    })
+    highscoreDiv.appendChild(ul);
+    
   }
   static createUserConfig(userName, userHighscore = 0) {
     return {
@@ -64,6 +71,7 @@ class User {
             }})
     }
   }
+
   static updateUserConfig(id, name = "", highscore) {
         return {
   
@@ -81,6 +89,7 @@ class User {
     }
     
   }
+
   static destroyUserConfig(id) {
 
     return {
