4a7b062f486b3cf4693d9e6a646e2f1f25b05ebb
Lrd134 <larryc3200@gmail.com>
2021-09-29 22:11:01 -0400

    updated destroy route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, except: %i[show update]
+  resources :users, except: %i[show update destroy edit]
   scope '/users' do 
     get '/:name', to: 'users#show'
     post '/:name', to: 'users#update'
+    delete '/:name', to: 'users#destroy'
   end
 end
