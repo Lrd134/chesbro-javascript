8533d1f42133932cfbf9fc582faa41d9114636a9
Lrd134 <larryc3200@gmail.com>
2021-10-07 16:50:17 -0400

    ammended failure method

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::API
   private
     def shouldFail?
       t = Time.now
-      t.min % 5 === 0 || t.sec % 3 === 0 ? true : false
+      t.sec % 3 === 0 ? true : false
     end
 end
