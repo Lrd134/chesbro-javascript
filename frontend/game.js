9e406913203c10c37f14d6844528799aecba0a3f
Lrd134 <larryc3200@gmail.com>
2021-10-11 22:50:54 -0400

    level 1 enemies now move

--- frontend/game.js
+++ frontend/game.js
@@ -1,18 +1,25 @@
 class Enemy {
-  constructor(x = 150, y = 150, lw = 10) {
+  constructor(x = 150, y = 150, lw = 15) {
     this.x = x;
     this.y = y;
-    this.dx = -lw;
-    this.dy = -lw;
+    this.dx = lw;
+    this.dy = lw;
     this.startX = x;
     this.startY = y;
     this.lw = lw;
+    this.tick = 1;
     this.move = () => {
-      if (this.x < this.startX + 200 && this.y < this.startY + 200) {
-        debugger;
-        debugger;
-        debugger;
-      }
+      if (this.tick === 3) {
+        if (this.x < this.startX + 200 && this.x >= this.startX) {
+          this.x += this.dx;
+        }
+        else if (this.x > this.startX + 200 || this.x < this.startX) {
+          this.dx = this.dx * -1;
+          this.x += this.dx;
+        }
+        this.tick = 1;
+      } else if (this.tick <= 2)
+        this.tick += 1;
     }
   }
 }
