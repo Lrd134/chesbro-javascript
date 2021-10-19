9c82e12cc938f99138ec28eea86e72aa709e95a5
Lrd134 <larryc3200@gmail.com>
2021-10-19 02:51:08 -0400

    removed cheater buttons

--- frontend/index.js
+++ frontend/index.js
@@ -2,35 +2,16 @@
 const indexUrl = "http://localhost:3000/"
 
 const addGameButton = () => {
-  let [newButton, endButton, winButton] = [document.createElement('button'), document.createElement('button'), document.createElement('button')];
+  
+  let newButton = document.createElement('button');
+
   newButton.innerText = "New Game";
-  endButton.innerText = "End Game";
-  winButton.innerText = "Win Game";
+
   newButton.addEventListener('click', e => {
     new Game
   });
 
-  endButton.addEventListener('click', e => {
-    Game.player.x = 160;
-    Game.player.y = 160;
-  });
-  winButton.addEventListener('click', e=> {
-    Game.player.x = 640;
-    Game.player.y = 500;
-  })
-  
   Game.canvas.parentNode.insertBefore(newButton, Game.canvas);
-  
-  Game.canvas.parentNode.insertBefore(endButton, Game.canvas);
-  
-  Game.canvas.parentNode.insertBefore(winButton, Game.canvas);
-
-  Game.canvas.addEventListener('click', e => {
-    const boundCoords = Game.canvas.getBoundingClientRect();
-    const coords = Game.coordsInCanvas(e.clientX, e.clientY)
-    console.log(`X: ${coords.x}` + `\nY: ${coords.y}`);
-    console.log(`X: ${e.clientX}` + `\nY: ${e.clientY}`)
-  })
 }
 
 document.addEventListener("DOMContentLoaded", e =>{
