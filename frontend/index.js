e64445e51af52839a0ca620b9edd707463074f1c
Lrd134 <larryc3200@gmail.com>
2021-10-19 03:43:04 -0400

    styled new game button

--- frontend/index.js
+++ frontend/index.js
@@ -6,9 +6,12 @@ const addGameButton = () => {
   let newButton = document.createElement('button');
 
   newButton.innerText = "New Game";
+  newButton.classList.add('game');
 
   newButton.addEventListener('click', e => {
     new Game
+    Game.canvas.style.marginTop = '250px;'
+    newButton.remove();
   });
 
   Game.canvas.parentNode.insertBefore(newButton, Game.canvas);
