fca962d159f7ff29e4569b03cccf967d7e62fa39
Lrd134 <larryc3200@gmail.com>
2022-01-21 17:45:13 -0500

    enemy does not handle 'ticks'

--- frontend/enemy.js
+++ frontend/enemy.js
@@ -8,21 +8,16 @@ export default class Enemy {
     this.startX = x;
     this.startY = y;
     this.lw = lw;
-    this.tick = 1;
 
     this.move = () => {
 
-      if (this.tick === 3) {
-        if (this.x < this.startX + 200 && this.x >= this.startX) {
-          this.x += this.dx;
-        }
-        else if (this.x > this.startX + 200 || this.x < this.startX) {
-          this.dx = this.dx * -1;
-          this.x += this.dx;
-        }
-        this.tick = 1;
-      } else if (this.tick <= 2)
-        this.tick += 1;
+      if (this.x < this.startX + 200 && this.x >= this.startX) {
+        this.x += this.dx;
+      }
+      else if (this.x > this.startX + 200 || this.x < this.startX) {
+        this.dx = this.dx * -1;
+        this.x += this.dx;
+      }
     }
   }
 }
