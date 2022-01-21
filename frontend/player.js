9557969c03c33cccc316fa05d3c073d7dfadeb2b
Lrd134 <larryc3200@gmail.com>
2022-01-21 17:15:55 -0500

    added movement param

--- frontend/player.js
+++ frontend/player.js
@@ -2,40 +2,42 @@
 export default class Player {
   static current_player;
 
-  constructor(x = 8, y = 8, radius = 4) {
+  constructor(x = 8, y = 8, radius = 4, mvAmnt = 8) {
     this.x = x;
     this.y = y;
-    this.dx = 8;
-    this.dy = 8;
+    this.dx = mvAmnt;
+    this.dy = mvAmnt;
     this.radius = radius;
     Player.current_player = this;
   }
+
   move(e) {
+
     switch(e.key){
 
-    case ('a' || 'ArrowLeft'): {
-      if (Player.current_player.x > 8)
-        Player.current_player.x -= this.dx;
-      break;
-    }
-    
-    case('d' || 'ArrowRight'): {
-      if (Player.current_player.x < 472)
-        Player.current_player.x += this.dx;
-      break;
-    }
+      case ('a' || 'ArrowLeft'): {
+        if (Player.current_player.x > 8)
+          Player.current_player.x -= this.dx;
+        break;
+      }
+      
+      case('d' || 'ArrowRight'): {
+        if (Player.current_player.x < 472)
+          Player.current_player.x += this.dx;
+        break;
+      }
 
-    case('w' || 'ArrowUp'): {
-      if (Player.current_player.y > 8)
-        Player.current_player.y -= this.dy;
-      break;
-    }
+      case('w' || 'ArrowUp'): {
+        if (Player.current_player.y > 8)
+          Player.current_player.y -= this.dy;
+        break;
+      }
 
-    case('s' || 'ArrowDown'): {
-      if (Player.current_player.y < 312)
-        Player.current_player.y += this.dy;
-      break;
-    }
+      case('s' || 'ArrowDown'): {
+        if (Player.current_player.y < 312)
+          Player.current_player.y += this.dy;
+        break;
+      }
     }
   }
 }
