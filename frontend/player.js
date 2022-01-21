57b2bb176bcbb03f1f5941dd6b3f20ad0203e5fa
Lrd134 <larryc3200@gmail.com>
2022-01-21 18:17:07 -0500

    fixed arrow key movement

--- frontend/player.js
+++ frontend/player.js
@@ -12,28 +12,27 @@ export default class Player {
   }
 
   move(e) {
-
-    switch(e.key){
-
-      case ('a' || 'ArrowLeft'): {
+    switch(e.keyCode){
+      case(37):
+      case (65): {
         if (Player.current_player.x > 8)
           Player.current_player.x -= Player.current_player.dx;
         break;
       }
-      
-      case('d' || 'ArrowRight'): {
+      case(39):
+      case(68): {
         if (Player.current_player.x < 472)
           Player.current_player.x += Player.current_player.dx;
           break;
       }
-
-      case('w' || 'ArrowUp'): {
+      case(38):
+      case(87): {
         if (Player.current_player.y > 8)
           Player.current_player.y -= Player.current_player.dy;
           break;
       }
-
-      case('s' || 'ArrowDown'): {
+      case(40):
+      case(83): {
         if (Player.current_player.y < 312)
           Player.current_player.y += Player.current_player.dy;
           break;
