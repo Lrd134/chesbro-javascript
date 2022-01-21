21c6948a6aeb978ea46048c347334d9db99fcbcc
Lrd134 <larryc3200@gmail.com>
2022-01-21 17:08:29 -0500

    refactored player

--- frontend/player.js
+++ frontend/player.js
@@ -2,63 +2,41 @@
 export default class Player {
   static current_player;
 
-  constructor(x = 8, y = 8, radius= 4) {
+  constructor(x = 8, y = 8, radius = 4, mvAmnt = 8) {
     this.x = x;
     this.y = y;
+    this.dx = mvAmnt;
+    this.dy = mvAmnt;
     this.radius = radius;
     Player.current_player = this;
   }
   move(e) {
+
     switch(e.key){
 
-    case ('a'): {
-      if (Player.current_player.x > 8)
-        Player.current_player.x -= Game.dx;
-      break;
-    }
-    
-    case('d'): {
-      if (Player.current_player.x < 472)
-        Player.current_player.x += Game.dx;
-      break;
-    }
+      case ('a' || 'ArrowLeft'): {
+        if (Player.current_player.x > 8)
+          Player.current_player.x -= this.dx;
+        break;
+      }
+      
+      case('d' || 'ArrowRight'): {
+        if (Player.current_player.x < 472)
+          Player.current_player.x += this.dx;
+        break;
+      }
 
-    case('w'): {
-      if (Player.current_player.y > 8)
-        Player.current_player.y -= Game.dy;
-      break;
-    }
+      case('w' || 'ArrowUp'): {
+        if (Player.current_player.y > 8)
+          Player.current_player.y -= this.dy;
+        break;
+      }
 
-    case('s'): {
-      if (Player.current_player.y < 312)
-        Player.current_player.y += Game.dy;
-      break;
-    }
-
-    case('ArrowRight'): {
-      if (Player.current_player.x < 472)
-        Player.current_player.x += Game.dx;
-      break;
-    }
-
-    case('ArrowLeft'): {
-      if (Player.current_player.x > 8)
-        Player.current_player.x -= Game.dx;
-      break;
-    }
-
-    case('ArrowUp'): {
-      if (Player.current_player.y > 8)
-        Player.current_player.y -= Game.dy;
-      break;
-    }
-
-    case('ArrowDown'): {
-      if (Player.current_player.y < 312)
-        Player.current_player.y += Game.dy;
-      break;
-    }
+      case('s' || 'ArrowDown'): {
+        if (Player.current_player.y < 312)
+          Player.current_player.y += this.dy;
+        break;
+      }
     }
   }
-
 }
